40ac0c4d0744b34db96296a8739c905c3d721a68
Matthew Downs <mwdowns@gmail.com>
2016-11-14 21:10:36 -0500

    memory game using angular

--- memory_game.js
+++ memory_game.js
@@ -1,66 +1,98 @@
-var app = angular.module('my-app', []);
-app.controller('memoryGame', function($scope, $timeout){
+function Card(num){
+  this.url= "images/monsters-"+ num+".png";
+  this.open = false;
+  this.matched = false;
+}
+function Board(){
+  this.protoboard = [];
+  this.board = [];
+  this.size= 0;
+}
 
-  function Card(num){
-    this.url= "images/monsters-"+ num+".png";
-    this.open = false;
-    this.matched = false;
-  }
-  function Board(){
-    // this.board = [[],[]];
-    this.protoboard = [];
-    this.board = [];
-    this.size= 0;
-  }
-
-  function shuffle(board) {
-     var i = 0;
-     var j = 0;
-     var temp = null;
-
-     for (i = board.length - 1; i > 0; i--) {
-       j = Math.floor(Math.random() * (i + 1));
-       temp = board[i];
-       board[i] = board[j];
-       board[j] = temp;
-     }
-    }
-
-  Board.prototype.makeBoard = function(board_size) {
+Board.prototype.makeBoard = function(board_size) {
     var num = (board_size / 2);
-    // var rand = Math.floor(Math.random() * 16) + 1;
+    var randNums = [];
     for (var i = 0; i < num; i++) {
       var rand = Math.floor(Math.random() * 16) + 1;
+      randNums.push(rand);
+      console.log(randNums);
+      if (randNums.indexOf(rand) === -1) {
+      }
       this.protoboard.push(new Card(rand));
       this.protoboard.push(new Card(rand));
+      console.log(this.protoboard);
     }
     shuffle(this.protoboard);
-    var board1 = this.protoboard.splice(0, num);
-    this.board.push(board1);
+    if (8) {
+    var easyboard1 = this.protoboard.splice(0, num);
+    this.board.push(easyboard1);
     this.board.push(this.protoboard);
+    }
+    else if (18) {
+      var mediumboard1 = this.protoboard.splice(0, 6);
+      var mediumboard2 = this.protoboard.splice(0, 6);
+      this.board.push(mediumboard1);
+      this.board.push(mediumboard2);
+      this.board.push(this.protoboard);
+    }
+    else if (32) {
+      var hardboard1 = this.protoboard.splice(0, 8);
+      var hardboard2 = this.protoboard.splice(0, 8);
+      var hardboard3 = this.protoboard.splice(0, 8);
+      this.board.push(hardboard1);
+      this.board.push(hardboard2);
+      this.board.push(hardboard3);
+      this.board.push(this.protoboard);
+    }
     this.board.size = num;
   };
 
-  // Board.prototype.makeBoard = function(board_size){
-  //   for(var i = 1; i < 5; i++){
-  //     for(var j = 0; j < board_size; j++ ){
-  //       this.board[j].push(new Card(i));
-  //     }
-  //   }
-  // };
+function shuffle(board) {
+   var i = 0;
+   var j = 0;
+   var temp = null;
 
-$scope.newGame = function(){
+   for (i = board.length - 1; i > 0; i--) {
+     j = Math.floor(Math.random() * (i + 1));
+     temp = board[i];
+     board[i] = board[j];
+     board[j] = temp;
+   }
+  }
+
+var app = angular.module('my-app', []);
+app.controller('memoryGame', function($scope, $timeout){
+
+$scope.chooseGame = function(){
+  $scope.chosen = true;
+};
+
+$scope.newGame = function(x){
   $scope.board = new Board();
-  $scope.board.makeBoard(8);
+  if(x === 'easy'){
+    $scope.board.makeBoard(8);
+  }
+  else if(x === 'medium'){
+    $scope.board.makeBoard(18);
+  }
+  else if(x === 'hard'){
+    $scope.board.makeBoard(32);
+  }
   $scope.state = 0;
   $scope.matches = [];
   $scope.firstcard = {};
   $scope.win_count = 0;
   $scope.winner = false;
+  $scope.chosen = false;
 };
 
-  $scope.newGame();
-  console.log($scope.board);
+$scope.checkWinner = function(){
+  if($scope.win_count === $scope.board.board.size){
+  $scope.winner = true;
+  }
+};
+
+  $scope.chooseGame();
   $scope.clicked = function(item) {
     if (item.open === false && $scope.state === 0) {
       item.open = true;
@@ -95,11 +127,4 @@ $scope.newGame = function(){
       },1300);
     }
   };
-
-  $scope.checkWinner = function(){
-    if($scope.win_count === $scope.board.board.size){
-    $scope.winner = true;
-    }
-  };
-
 });
